97196027c82fbc6b05b81be8ebf8457e40120df7
IssacJiang11 <issacjiang@microsoft.com>
2024-12-05 10:55:20 +0800

    initial update

---
+++ OutlookDialogAddinWeb/ResultPage/success.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global global, Office, self, window */
+
+/*Office.onReady(() => {
+    // If needed, Office.js is ready to be called
+$(document).attr("title", "File123 It");
+  });
+*/
